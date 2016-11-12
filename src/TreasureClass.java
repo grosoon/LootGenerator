99b3100d291b1a9fdba1e0424014f80176299914
Greyson H. Bourgeois <bourgeoi1@cs.grinnell.edu>
2016-11-12 14:23:37 -0600

    saved treasure class class

--- src/TreasureClass.java
+++ src/TreasureClass.java
@@ -1,4 +1,27 @@
+import java.util.Random;
 
 public class TreasureClass {
-
+	private String name;
+	private String[] items;
+	
+	
+	public TreasureClass(String s){
+		String[] inputs = s.split("\t");
+		name = inputs[0];
+		items = new String[3];
+		for(int i = 0; i < 3; i++){
+			items[i] = inputs[i+1];
+		}
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getRandomItem(){
+		Random rand = new Random();
+		return items[rand.nextInt(3)];
+	}
+	
+	
 }
