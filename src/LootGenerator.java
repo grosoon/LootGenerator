5f0a0bbdb418f467e5df0ded2a79edb8f7854391
Greyson H. Bourgeois <bourgeoi1@cs.grinnell.edu>
2016-11-12 14:22:54 -0600

    finished monster and treasureclass classes

--- src/LootGenerator.java
+++ src/LootGenerator.java
@@ -1,4 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class LootGenerator {
+	public List<Monster> getMonsters(File file) throws FileNotFoundException{
+		Scanner in = new Scanner(file);
+		List<Monster> ret = new ArrayList<Monster>();
+		while(in.hasNext()){
+			ret.add(new Monster(in.nextLine()));
+		}
+		
+		return ret;
+	}
+	
+	public List<TreasureClass> getTreasureClasses(File file) throws FileNotFoundException{
+		Scanner in = new Scanner(file);
+		List<TreasureClass> ret = new ArrayList<TreasureClass>();
+		while(in.hasNext()){
+			ret.add(new TreasureClass(in.nextLine()));
+		}
+		
+		return ret;
+	}
+	
 
 }
---
+++ src/Monster.java
@@ -0,0 +1,23 @@
+
+public class Monster {
+	private String name;
+	private String treasureClass;
+	private String type;
+	private int level;
+	
+	public Monster(String s){	
+		String[] inputs = s.split("\t");
+		name = inputs[0];
+		type = inputs[1];
+		level = Integer.parseInt(inputs[2]);
+		treasureClass = inputs[3];
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getTreasureClass(){
+		return treasureClass;
+	}
+}
