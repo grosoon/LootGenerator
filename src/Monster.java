510025dfdf20d55f8671d41a44d9a8b07474b440
Dkyse <jingshid@grinnell.edu>
2016-11-12 13:26:20 -0600

    Add files via upload

---
+++ src/Monster.java
@@ -0,0 +1,14 @@
+
+public class Monster{
+/**
+ * This class is a data structure that stores the monster name and 
+ * corresponding treasure class.
+ * We use map structure.
+ */
+	
+	/**
+	 * Constructor #1: generate a Monster map without reading from files.
+	 */
+	
+	
+}
