af4398368accd69639cac5fe3224fbf6ff97c9ae
Dkyse <jingshid@grinnell.edu>
2016-11-12 14:39:35 -0600

    Add files via upload

--- src/LootGenerator.java
+++ src/LootGenerator.java
@@ -1,4 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class LootGenerator {
+	public static List<Monster> getMonsters(File file) throws FileNotFoundException{
+		Scanner in = new Scanner(file);
+		List<Monster> ret = new ArrayList<Monster>();
+		while(in.hasNext()){
+			ret.add(new Monster(in.nextLine()));
+		}
+		in.close();
+		return ret;
+	}
+	
+	public static List<TreasureClass> getTreasureClasses(File file) throws FileNotFoundException{
+		Scanner in = new Scanner(file);
+		List<TreasureClass> ret = new ArrayList<TreasureClass>();
+		while(in.hasNext()){
+			ret.add(new TreasureClass(in.nextLine()));
+		}
+		in.close();
+		return ret;
+	}
+	
+	public static void main(String[] argc) throws FileNotFoundException  {
+		File monster = new File("src/data/large/monstats.txt");
+		List<Monster> list = getMonsters(monster);
+		Monster m1 = list.get(0);
+		System.out.println(m1.getName());
+		System.out.println(m1.getTreasureClass());
+		
+	}
 
 }
--- src/Monster.java
+++ src/Monster.java
@@ -1,14 +1,22 @@
+public class Monster {
+	private String name;
+	private String treasureClass;
+	private String type;
+	private int level;
 
-public class Monster{
-/**
- * This class is a data structure that stores the monster name and 
- * corresponding treasure class.
- * We use map structure.
- */
-	
-	/**
-	 * Constructor #1: generate a Monster map without reading from files.
-	 */
-	
-	
+	public Monster(String s){	
+		String[] inputs = s.split("\t");
+		name = inputs[0];
+		type = inputs[1];
+		level = Integer.parseInt(inputs[2]);
+		treasureClass = inputs[3];
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public String getTreasureClass(){
+		return treasureClass;
+	}
 }
